b564e1f873b0b709924190860f41eec4bf9c1c5e
Clinton Dsouza <cvdsouza@asu.edu>
2016-09-03 20:29:46 -0400

    update 10

--- otx_alienvault_service/__init__.py
+++ otx_alienvault_service/__init__.py
@@ -537,7 +537,7 @@ class AlienVaultOTXService(Service):
                     for i in data_pe32info['pdbinfo']:
                         self._add_result("PE32Info : pdb info", richhash, i)
             else:
-                self._info("Info : "+ str(data_pe32info['pdbinfo']))
+                self._info("Info : "+ richhash)
                 self._add_result("PE32Info : pdb info", "".join(str(data_pe32info['pdbinfo'])))
 
 
