fcf988e5f3c27df351dc94cac7bb3c7c2bb98716
Clinton Dsouza <cvdsouza@asu.edu>
2016-10-02 22:54:54 -0400

    Added Author: Clinton Dsouza

--- punchipreputation_service/__init__.py
+++ punchipreputation_service/__init__.py
@@ -10,6 +10,8 @@ from django.template.loader import render_to_string
 from . import forms
 from crits.services.core import Service, ServiceConfigError
 
+__author__ =  "Clinton Dsouza"
+
 logger = logging.getLogger(__name__)
 
 class PunchService(Service):
